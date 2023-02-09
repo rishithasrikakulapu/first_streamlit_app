0e54a4a2e6519a2dcdf3e28f3257da4f6aa41fb9
rishithasrikakulapu <83440215+rishithasrikakulapu@users.noreply.github.com>
2023-02-08 19:09:26 -0500

    Update streamlit_app.py
    adding function to reuse code

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,16 +25,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  # normalizing the json table to look better
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  # converting normalized data into dataframe
+  return fruityvice_normalized
+  
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    # normalizing the json table to look better
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # converting normalized data into dataframe
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+    
 except URLError as e:
   streamlit.error()
 
